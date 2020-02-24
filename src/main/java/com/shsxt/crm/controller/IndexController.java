5e0a1dd383eb07507806aa55a666a2139071b141
lxy <1501312596@qq.com>
2020-02-24 14:58:56 +0800

    crm项目初始化

---
+++ src/main/java/com/shsxt/crm/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.shsxt.crm.controller;
+
+import com.shsxt.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController  extends BaseController {
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("main")
+    public String  main(){
+        return "main";
+    }
+}
